eae94d21b6cd607d11721d7e59cc07c8a6bb26a4
silastassin9-afk <silastassin9@gmail.com>
2025-09-18 01:09:40 +0000

    Player is here haha

---
+++ player/player.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <player/player.h>
+
+void Player::talk() {
+    isTalking = false;
+    if (isTalking = true) {
+        printf("Talking ...");
+    } else {
+        printf("Not talking");
+    }
+}
